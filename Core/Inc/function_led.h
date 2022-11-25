26a76e61ca78c511fa4c71fc93ce30ff5215490b
Bao Quoc <s2chungbeov2@gmail.com>
2022-11-25 19:45:52 +0700

    oki
    compelete

---
+++ Core/Inc/function_led.h
@@ -0,0 +1,18 @@
+/*
+ * function_led.h
+ *
+ *  Created on: Nov 24, 2022
+ *      Author: Bao Quoc
+ */
+
+#ifndef INC_FUNCTION_LED_H_
+#define INC_FUNCTION_LED_H_
+
+ void blink_RED ( void ) ;
+ void blink_YELLOW ( void ) ;
+ void blink_GREEN ( void );
+ void blink_BLUE ( void ) ;
+ void blink_PURPLE ( void ) ;
+
+
+#endif /* INC_FUNCTION_LED_H_ */
